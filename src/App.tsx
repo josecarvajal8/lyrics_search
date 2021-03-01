06e249664f3e21ab98b444e8f8a00646bb06af95
Jose Carvajal <josecarvajal@joses-mbp.lan>
2021-02-28 20:08:09 -0500

    setup base changes TS

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from 'react';
+import { Text, View } from 'react-native';
+const App: () => ReactNode = () => {
+    return (
+        <View>
+            <Text>
+                {'Hi Im the lyrics hunter'}
+            </Text>
+        </View>
+    )
+}
+
+export default App;
